f38f6c8425a30e8ae529e804f6a21079875a8aef
scoutfullner <scoutfullner96@gmail.com>
2022-01-05 19:14:22 -0800

    More vault bug fixes

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -3,8 +3,6 @@ pragma solidity >=0.7.0 <0.9.0;
 //SPDX-License-Identifier: UNLICENSED
 
 import "./World.sol";
-import "./Weapon.sol";
-import "./Boss.sol";
 import "./enums/DamageType.sol";
 import "./enums/WeaponType.sol";
 import "./enums/ItemTier.sol";
@@ -14,7 +12,7 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 contract Vault {
     
-    using EnumerableSet for EnumerableSet.AddressSet;
+    using EnumerableSet for EnumerableSet.UintSet;
     
     World constant private WORLD = World(0x992DA8eC2af8ec58E89E3293Fb3aaC8ebD7602B8); //TODO Update this with Actual Contract
     
@@ -27,11 +25,11 @@ contract Vault {
         _;
     }
 
-    function getDamageType(WeaponType _weaponType) public view returns (uint) {
+    function getDamageType(WeaponType _weaponType) public view returns (DamageType) {
         return weaponDamageTypes[_weaponType];
     }
   
-    function getWeaponTypes() public view returns (uint[]) {
+    function getWeaponTypes() public view returns (uint[] memory) {
         return weaponTypes.values();
     }
 
@@ -41,7 +39,7 @@ contract Vault {
     }
 
     function addWeapon(WeaponType _weaponType, DamageType _damageType) public returns (bool success) {
-        success = weaponTypes.add(_weaponType);
+        success = weaponTypes.add(uint(_weaponType));
         weaponDamageTypes[_weaponType] = _damageType;
     }
     
